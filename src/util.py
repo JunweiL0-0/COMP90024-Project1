a81d3b4b2abe2a63729082039c9d5b5ef37b8d6d
JunweiL0-0 <jli328@uclive.ac.nz>
2023-03-29 13:52:03 +1100

    count number of tweet, updated the assignment specification

--- src/util.py
+++ src/util.py
@@ -1,17 +1,10 @@
 import json
+import ijson
 
 SEPARATER = '*' * 5
 
-def print_num_processor(comm_size):
-    """
-    :param comm_size: number of processor
 
-    Print the number of processors
-    """
-    print(f"{SEPARATER} Running on {comm_size} processors {SEPARATER}")
-
-
-def is_json(myjson):
+def is_json(myjson: str):
     """
     return true if the input is a json string, return false otherwise
     """
@@ -20,3 +13,22 @@ def is_json(myjson):
     except ValueError as e:
         return False
     return True
+
+def get_num_of_tweet(twitter_file_path: str):
+    """
+    :param a string represent the path to the json file
+    :return number of items in the json file
+    """
+    counter = 0
+    with open(twitter_file_path, "rb") as f:
+        for tweet in ijson.items(f, "item"):
+            counter += 1
+    return counter
+
+def print_num_process(comm_size: int):
+    """
+    :param comm_size: number of processor
+
+    Print the number of processors
+    """
+    print(f"{SEPARATER} Running on {comm_size} process {SEPARATER}")

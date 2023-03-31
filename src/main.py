e8bc373585dc6372411e35368a8e1da31588517e
JunweiL0-0 <jli328@uclive.ac.nz>
2023-03-31 22:24:48 +1100

    Changed bigjson to readlines and Counter, edit slurm file, remove requirements

--- src/main.py
+++ src/main.py
@@ -5,33 +5,47 @@ Description: This main function is designed for analyzing the twitter data
 
 # Import mpi so we can run on more than one node and processor
 from mpi4py import MPI
-import sys, time, util
+from collections import defaultdict, Counter
+import sys, time, util, operator
 
 
 def master_processor(comm, twitter_file_path):
-    comm_rank = comm.Get_rank()
-    comm_size = comm.Get_size()
+    # Init parameters
+    comm_rank, comm_size = comm.Get_rank(), comm.Get_size()
+    author_tweet_counter = Counter()
+
     # Print num of processors
     util.print_num_process(comm_size)
-    # Get tweet generator
-    tweet_generator = util.get_tweet_genereator(twitter_file_path, comm_size)
-    # Loop through generator and get tweet for the processor
-    for index, tweet in tweet_generator:
-        is_my_tweet = index == comm_rank
-        if is_my_tweet:
-            print(f"Processor{comm_rank}, {tweet['_id']}")
-    
+    # Get start, end position
+    start_position, end_position = util.get_start_end_position(twitter_file_path, comm_rank, comm_size)
+    # Get line generator
+    line_generator = util.get_line_genereator(twitter_file_path, start_position, end_position)
+    # Loop throught lines
+    for line in line_generator:
+        if util.have_author_id(line):
+            # If this line contains author id, we store them into the author_tweet_counter
+            author_tweet_counter[int(util.get_author_id(line))] += 1
+    # Reduce to root and merge the dictionary. Set commute to true: dict1 + dict2 = dict2 + dict1
+    reduced_author_counter = comm.reduce(author_tweet_counter, root=0, op=operator.add)
+    # Check if we got the value from other processors
+    print(reduced_author_counter.items())
 
 def slave_processor(comm, twitter_file_path):
-    comm_rank = comm.Get_rank()
-    comm_size = comm.Get_size()
-    # Get tweet generator
-    tweet_generator = util.get_tweet_genereator(twitter_file_path, comm_size)
-    # Loop through generator and get tweet for the processor
-    for index, tweet in tweet_generator:
-        is_my_tweet = index == comm_rank
-        if is_my_tweet:
-            print(f"Processor{comm_rank}, {tweet['_id']}")
+    # Init parameters
+    comm_rank, comm_size = comm.Get_rank(), comm.Get_size()
+    author_tweet_counter = Counter()
+
+    # Get start, end position
+    start_position, end_position = util.get_start_end_position(twitter_file_path, comm_rank, comm_size)
+    # Get line generator
+    line_generator = util.get_line_genereator(twitter_file_path, start_position, end_position)
+    # Loop throught lines
+    for line in line_generator:
+        if util.have_author_id(line):
+            # If this line contains author id, we store them into the author_tweet_counter
+            author_tweet_counter[int(util.get_author_id(line))] += 1
+    # Reduce to root and merge the dictionary. Set commute to true: dict1 + dict2 = dict2 + dict1
+    reduced_author_counter = comm.reduce(author_tweet_counter, root=0, op=operator.add)
 
 def main(twitter_file_path):
     """
--- src/util.py
+++ src/util.py
@@ -1,30 +1,59 @@
-import json
-import bigjson
+import json, os, math, ijson
 
 SEPARATER = '*' * 5
+    
 
+def get_start_end_position(twitter_file_path, comm_rank, comm_size):
+    # Total file size
+    total_file_size = os.stat(twitter_file_path).st_size
+    # Split into pieces: Say we have four prpcossers, 
+    chunk_size =  total_file_size // comm_size
+    # Starting position
+    start_position = chunk_size * comm_rank
+    # Ending position
+    end_postion = chunk_size * comm_rank
+    if comm_rank == 0:
+        # Master processor
+        start_position, end_position = 0, chunk_size
+    elif comm_rank != 0 and comm_rank != (comm_size - 1):
+        # Other processor. Not the last one
+        start_position, end_position = chunk_size * comm_rank, (chunk_size * (comm_rank + 1))
+    else:
+        # Other processor. The last one will read till the the EOF
+        start_position, end_position = chunk_size * comm_rank, total_file_size
+    # print(start_position, end_position)
+    return start_position, end_position
 
-def is_json(myjson: str):
+def get_line_genereator(twitter_file_path, start_position, end_position):
     """
-    return true if the input is a json string, return false otherwise
-    """
-    try:
-        json.loads(myjson)
-    except ValueError as e:
-        return False
-    return True
+        :param a string represent the path to the json file
+        :return number of items in the json file
 
-def get_tweet_genereator(twitter_file_path: str, comm_size: int):
+        |_________
+                 |__________
+                           |__________(ignore the very end line and return None)
     """
-    :param a string represent the path to the json file
-    :return number of items in the json file
-    """
-    with open(twitter_file_path, "rb") as f:
-        json = bigjson.load(f)
-        index = 0
-        for tweet in json:
-            index = ((index + 1) % comm_size)
-            yield (index, tweet)
+    with open(twitter_file_path, "r") as f:
+        # Init variable
+        line = 0
+        # Navigate to staring position
+        f.seek(start_position)
+        # Only process the lines we want
+        while True:
+            # One line per time
+            line = f.readline()
+            if f.tell() < end_position:
+                yield line
+            else:
+                return None
+
+def get_author_id(input_string):
+    # Json file have fixed format, we can use the slicing to retrive author id
+    return input_string[20:-3]
+
+def have_author_id(input_string):
+    author_id_format = "      \"author_id\": "
+    return author_id_format in input_string
 
 def print_num_process(comm_size: int):
     """
@@ -38,3 +67,17 @@ def print_elapsed_time(end_time, start_time):
     # Output running time on master processor
     elapsed = end_time - start_time
     print(f"Porgram elapsed time: {elapsed:.10f}")
+
+def sum_two_defaultdict(dict1, dict2, datatype):
+    for key in dict2.keys():
+        dict1[key] += dict2[key]
+    return dict1
+
+def solve_first_question(reduced_author_counter):
+    # Get first ten
+    print("Question1: Top 10 tweeters in terms of the number of tweets made irrespective of where they tweeted")
+    print(f'{"Rank":<13}  {"AuthorID":<25}  {"Num Of Tweet Made":<12}')
+    rank = 1
+    for author_id, num_of_tweet in reduced_author_counter.most_common(10):
+        print(f'#{rank:<12}  {author_id:<25}  {num_of_tweet:<12}')
+        rank += 1

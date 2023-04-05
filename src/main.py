0bd88e04e050dacd4e6030d863813ae7fb8ebcbd
JunweiL0-0 <jli328@uclive.ac.nz>
2023-04-05 14:21:08 +1000

    Ready to test

--- src/main.py
+++ src/main.py
@@ -5,69 +5,79 @@ Description: This main function is designed for analyzing the twitter data
 
 # Import mpi so we can run on more than one node and processor
 from mpi4py import MPI
-from collections import Counter
-import sys, time, util, operator, json
+from collections import Counter, defaultdict
+import sys, time, util, operator
 
 
-def master_processor(comm, twitter_file_path, sal_file_path):
+def master_processor(comm, twitter_files_path, sal_file_path):
     # Init parameters
     comm_rank, comm_size = comm.Get_rank(), comm.Get_size()
     util.print_num_process(comm_size)
-    author_counter = Counter()
-    number_of_tweet_counter = Counter()
-    master_tweet_list = util.get_all_tweet(twitter_file_path)
-    print("Total length", len(master_tweet_list))
-
-    # Distribute the work to all the workers, if we have extra worker
-    if comm_size > 1:
-        util.distribute_work_to_worker(comm, master_tweet_list)
-
-    # Reduce the size of the list to save some memory and get ready for analyze
-    master_tweet_list = master_tweet_list[:len(master_tweet_list)//comm_size]
+    question1_counter = Counter()
+    question2_counter = Counter()
+    question3_dict = defaultdict(Counter)
+    unknown = set()
+    # Get all tweet
+    master_tweet_list = util.get_all_tweet(twitter_files_path[comm_rank])
+    # Get the sal list
+    master_sal_list = util.get_sal_data_list(sal_file_path)
 
     # Counte the number of tweet for each author id
     for tweet in master_tweet_list:
-        author_counter[tweet.author_id] += 1
+        author_id = tweet['author_id']
+        place_full_name = tweet['place_full_name']
+        # Question 1
+        question1_counter[author_id] += 1
+        # Question 2
+        is_in_gcc, gcc_code = util.is_in_gcc(place_full_name, master_sal_list)
+        if is_in_gcc:
+            question2_counter[gcc_code] += 1
+            question3_dict[author_id][gcc_code] += 1
+        else:
+            unknown.add(place_full_name)
+        # Question 3
+
     # Merge the author counter from all the workers
-    reduced_author_counter = comm.reduce(author_counter, op=operator.add, root=comm_rank)
-    util.solve_first_question(reduced_author_counter)
+    reduced_question1_counter = comm.reduce(question1_counter, op=operator.add, root=comm_rank)
+    reduced_question2_counter = comm.reduce(question2_counter, op=operator.add, root=comm_rank)
+    reduced_question3_dict = comm.reduce(question3_dict, op=MPI.Op.Create(util.add_default_dict, commute=True), root=comm_rank)
+    # reduced_unknown = comm.reduce(unknown, op=operator.add, root=0)
+    util.solve_first_question(reduced_question1_counter)
+    util.solve_second_question(reduced_question2_counter)
+    util.solve_third_question(reduced_question3_dict)
 
-    SAL_DATA = comm.bcast(util.get_sal_data_list(sal_file_path), root=comm_rank)
-
-    for tweet in master_tweet_list:
-        if tweet.place in SAL_DATA.keys():
-            number_of_tweet_counter[SAL_DATA[tweet.place]] += 1
-    print(number_of_tweet_counter)
-
-    print("Master length", len(master_tweet_list))
-
-
-def worker_processor(comm):
+def worker_processor(comm, twitter_files_path, sal_file_path):
     # Init parameters
     comm_rank, comm_size = comm.Get_rank(), comm.Get_size()
     master_node = 0
-    author_counter = Counter()
-    number_of_tweet_counter = Counter()
-    # Receiving the data, blocking
-    worker_tweet_list = comm.recv(source=master_node)
-
+    question1_counter = Counter()
+    question2_counter = Counter()
+    question3_dict = defaultdict(Counter)
+    unknown = set()
+    # Get all tweet
+    worker_tweet_list = util.get_all_tweet(twitter_files_path[comm_rank])
+    # Get the sal data
+    worker_sal_list = util.get_sal_data_list(sal_file_path)
+    
+    # Count number of tweet for each author
     for tweet in worker_tweet_list:
-        author_counter[tweet.author_id] += 1
-    # Merge the result back to the master node
-    comm.reduce(author_counter, op=operator.add, root=master_node)
-    SAL_DATA = comm.bcast(None, root=master_node)
+        author_id = tweet['author_id']
+        place_full_name = tweet['place_full_name']
+        # Question 1
+        question1_counter[author_id] += 1
+        # Question 2
+        is_in_gcc, gcc_code = util.is_in_gcc(place_full_name, worker_sal_list)
+        if is_in_gcc:
+            question2_counter[gcc_code] += 1
+            question3_dict[author_id][gcc_code] += 1
+        else:
+            unknown.add(place_full_name)
+    # Merge the result back to the master node to solve question 1
+    comm.reduce(question1_counter, op=operator.add, root=master_node)
+    comm.reduce(question2_counter, op=operator.add, root=master_node)
+    comm.reduce(question3_dict, op=MPI.Op.Create(util.add_default_dict, commute=True), root=master_node)
 
-    for tweet in worker_tweet_list:
-        # print(SAL_DATA.keys())
-        if tweet.place in SAL_DATA.keys():
-            number_of_tweet_counter[SAL_DATA[tweet.place]] += 1
-    # print(number_of_tweet_counter.items())
-         
-
-    print("Worker length", len(worker_tweet_list))
-
-
-def main(twitter_file_path, sal_file_path):
+def main(twitter_files_path, sal_file_path):
     """
     """
     # Start time
@@ -76,16 +86,16 @@ def main(twitter_file_path, sal_file_path):
     comm = MPI.COMM_WORLD
     rank = comm.Get_rank()
     if rank == 0:
-        master_processor(comm, twitter_file_path, sal_file_path)
+        master_processor(comm, twitter_files_path, sal_file_path)
         # Elapsed time
         util.print_elapsed_time(time.time(), program_start_time)
         # Terminates MPI execution env and exit the program
         MPI.Finalize()
     else:
-        worker_processor(comm)
+        worker_processor(comm, twitter_files_path, sal_file_path)
 
 if __name__ == "__main__":
     # run program
-    twitter_file_path = sys.argv[1]
+    twitter_files_path = sys.argv[1].split('|')
     sal_file_path = sys.argv[2]
-    main(twitter_file_path, sal_file_path)
+    main(twitter_files_path, sal_file_path)
--- src/util.py
+++ src/util.py
@@ -1,18 +1,7 @@
-import ijson, json
-from TwitterData import TwitterData
+import json, re
+from collections import defaultdict
 
 SEPARATER = '*' * 5
-
-def get_all_tweet(twitter_file_path):
-    """
-    :param twitter_file_path: A string represent the path to the twitter file
-    """
-    with open(twitter_file_path, "rb") as f:
-        result = []
-        all_tweet = ijson.items(f, "item")
-        for tweet in all_tweet:
-            result.append(TwitterData(tweet))
-        return result
 
 
 def print_num_process(comm_size: int):
@@ -23,6 +12,44 @@ def print_num_process(comm_size: int):
     """
     print(f"{SEPARATER} Running on {comm_size} processors {SEPARATER}")
 
+def get_all_tweet(twitter_file_path):
+    """
+    :param twitter_file_path: A string represent the path to the twitter file
+    """
+    with open(twitter_file_path, "rb") as f:
+        result = []
+        all_tweet = json.load(f)
+        return all_tweet
+
+def solve_first_question(reduced_question1_counter):
+    """
+    :param reduced_author_coutner
+    """
+    # Get first ten
+    print("Question1: Top 10 tweeters in terms of the number of tweets made irrespective of where they tweeted")
+    # Aligment for pretty priting
+    print(f'{"Rank":<13}  {"AuthorID":<25}  {"Number Of Tweet Made":<12}')
+    rank = 1
+    for author_id, num_of_tweet in reduced_question1_counter.most_common(10):
+        print(f'#{rank:<12}  {author_id:<25}  {num_of_tweet:<12}')
+        rank += 1
+
+def solve_second_question(reduced_question2_counter):
+    print("\nQuestion2: Return the number of tweets made in the various capital cities by all users.")
+    print(f'{"Greater Capital City":<20}  {"Number of tweets made":<25}')
+    for gcc, num_of_tweet in reduced_question2_counter.most_common(10):
+        print(f'{gcc:<20}  {num_of_tweet:<25}')
+
+def solve_third_question(question3_dict):
+    print("\nQuestion3: Tweeters that have tweeted in the most Greater Capital sities and the number of times they have tweeted from those locations. The top 10..")
+    print(f'#{"Rank":<12}  {"Author Id":<30}  {"Number of Unique City Locations and #Tweets":<12}')
+    rank = 1
+    # First by number of cities and second by the number of tweets
+    sorted_author_id = sorted(question3_dict.items(), key=lambda x: (len(x[1].keys()), sum(x[1].values())), reverse=True)[:10]
+    for author_id, tweet_counter in sorted_author_id:
+        print(f'#{rank:<12}  {author_id:<30}  {len(tweet_counter.keys()):<6} TotalTweets{sum(tweet_counter.values())} {tweet_counter.items()}')
+        rank += 1
+
 def print_elapsed_time(end_time, start_time):
     """
     :param end_time: ending time of the program
@@ -32,47 +59,68 @@ def print_elapsed_time(end_time, start_time):
     elapsed = end_time - start_time
     print(f"Porgram elapsed time: {elapsed:.10f}")
 
-def distribute_work_to_worker(comm, master_tweet_list):
-    """
-    :param comm: communicator
-    :param master_tweet_list: a list of TwitterData objects which contains all the tweet info
-    """
-    comm_rank, comm_size = comm.Get_rank(), comm.Get_size()
-    total_tweet = len(master_tweet_list)
-    # Calculate the chunck size base on the size
-    chunck_size = total_tweet // comm_size
-
-    # Send this to the worker nodes
-    for rank in range(1, comm_size):
-        # Send all the rest to the last node
-        if rank == comm_size - 1:
-            comm.send(master_tweet_list[chunck_size*(comm_size-1):], dest=rank)
-            continue
-        # Evenly split and send to other nodes
-        comm.send(master_tweet_list[chunck_size*rank : chunck_size*(rank+1)], dest=rank)
+def remove_bracket(input_string):
+    # Remove ' ()' and anything inside the bracket
+    target = r"\s*\([^)]*\)\s*"
+    empty_string = ""
+    return re.sub(target, empty_string, input_string)
 
 def get_sal_data_list(sal_file_path):
     # We only return the data we are interested in
-    targeted_gcc = {'7gdar', '6ghob', '4gade', '5gper', '2gmel', '1gsyd', '8acte', '9oter', '3gbri'}
-    result = dict()
+    targeted_gcc = {'1gsyd', '2gmel', '3gbri', '4gade', '5gper', '6ghob', '7gdar', '8acte', '9oter'}
+    result = defaultdict(set)
     with open("sal.json", 'r') as f:
         for i in json.load(f).items():
-            place_full_name = i[0]
+            # remove bracket ' ()' and all to lowercase and remove all white spaces before and after
+            place_full_name = remove_bracket(i[0]).lower().strip()
             gcc_code = i[1]["gcc"]
             if gcc_code in targeted_gcc:
-                result[gcc_code] = place_full_name
+                result[gcc_code].add(place_full_name)
         return result
-
-
-def solve_first_question(reduced_author_counter):
-    """
-    :param reduced_author_coutner
-    """
-    # Get first ten
-    print("Question1: Top 10 tweeters in terms of the number of tweets made irrespective of where they tweeted")
-    # Aligment for pretty priting
-    print(f'{"Rank":<13}  {"AuthorID":<25}  {"Num Of Tweet Made":<12}')
-    rank = 1
-    for author_id, num_of_tweet in reduced_author_counter.most_common(10):
-        print(f'#{rank:<12}  {author_id:<25}  {num_of_tweet:<12}')
-        rank += 1
+    
+def add_default_dict(dict1, dict2, datatype):
+    for key in dict1.keys():
+        dict1[key] += dict2[key]
+    for key in dict2.keys():
+        if key not in dict1.keys():
+            dict1[key] = dict2[key]
+    return dict1
+    
+def is_in_gcc(place_full_name, sal_list):
+    # We can predefind the gcc names here for later referencing. Except for 9oter
+    gcc = {'sydney':'1gsyd', 'melbourne':'2gmel', 'brisbane':'3gbri', 'adelaide':'4gade', 'perth':'5gper', 'hobart':'6ghob', 'darwin':'7gdar', 'canberra':'8acte'}
+    # Split it into parts and remove all white spaces before and after to find the exact match
+    place_full_name = [x.strip() for x in place_full_name.split(',')]
+    if len(place_full_name) == 1:
+        # Retrieve the full name
+        place_full_name = place_full_name[0]
+        if place_full_name in gcc.keys():
+            # If its name is the greater captial city name
+            return (True, gcc[place_full_name])
+        else:
+            # Else we loop through the sal list to find a match
+            for gcc_code, sal_place in sal_list.items():
+                if place_full_name in sal_place:
+                    # Return true and its code if there is a match
+                    return (True, gcc_code)
+            # Return false and None if we cant find one
+            return (False, None)
+    elif len(place_full_name) == 2:
+        # Retrieve the full name
+        place_full_name1, place_full_name2 = place_full_name[0], place_full_name[1]
+        if place_full_name1 in gcc.keys():
+            # If its first part is the greater captial city name
+            return (True, gcc[place_full_name1])
+        elif place_full_name2 in gcc.keys():
+            # If its second part is the greater captial city name
+            return (True, gcc[place_full_name2])
+        else:
+            # Else we loop through the sal list to find a match
+            for gcc_code, sal_place in sal_list.items():
+                if place_full_name1 in sal_place:
+                    return (True, gcc_code)
+                elif place_full_name2 in sal_place:
+                    return (True, gcc_code)
+            return (False, None)
+    else:
+        return (False, None)
